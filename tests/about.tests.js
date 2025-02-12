fa12d963ff1a45e2d68aaa7ec20d7d0162627ff9
yankovva <maggie.yankovaaa@gmail.com>
2025-02-12 14:33:58 +0100

    Change About test

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -6,6 +6,6 @@ suite('About page', function() {
     let res = await fetch("http://localhost:8888/about");
     let body = await res.text();
     assert.ok(body.includes("<title>About</title>"));
-    assert.ok(body.includes("<h1>About</h1>"));
+    assert.ok(body.includes("<h1>About DevOps</h1>"));
   });
 });
